337282888d9094523975c71bb92ec3788ed671cf
Mahendra Dinata <remdinata@gmail.com>
2024-01-10 22:04:47 +0700

    add file appender for log

--- src/main.rs
+++ src/main.rs
@@ -19,18 +19,24 @@ fn database_url() -> String {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let _ = fs::create_dir("./files");
+
+    let file_appender = tracing_appender::rolling::hourly("./files/log/", "wizard.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+
     let subscriber = tracing_subscriber::fmt()
         .compact()
         .with_thread_ids(true)
         .with_target(true)
+        .with_writer(non_blocking)
         .finish();
+
     tracing::subscriber::set_global_default(subscriber)?;
 
     let url = database_url();
     let pool = sqlx::PgPool::connect(&url).await?;
 
     sqlx::migrate!().run(&pool).await?;
-    let _ = fs::create_dir("./files");
 
     let api_service = OpenApiService::new(wizard_api::WizardApi, "Wizard API", "1.0.0");
 
@@ -43,6 +49,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .with(Cors::new())
         .data(pool);
 
+    println!("Server starting at port 3000");
     poem::Server::new(TcpListener::bind("0.0.0.0:3000"))
         .run(app)
         .await?;
