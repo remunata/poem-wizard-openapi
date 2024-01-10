f9d67bb51536f85db7745f383ddb115e8c3ff590
Mahendra Dinata <remdinata@gmail.com>
2024-01-10 17:16:25 +0700

    add dockerfile

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,24 @@ mod wizard_api;
 mod wizard_responses;
 mod wizard_service;
 
+fn database_url() -> String {
+    let user = std::env::var("PG_USER").unwrap_or_else(|_| "postgres".into());
+    let password = std::env::var("PG_PASSWORD").unwrap_or_else(|_| "postgrespass".into());
+    let host = std::env::var("PG_HOST").unwrap_or_else(|_| "localhost".into());
+    let dbname = std::env::var("PG_DBNAME").unwrap_or_else(|_| "poem_wizard_openapi".into());
+
+    format!("postgres://{}:{}@{}/{}", user, password, host, dbname).to_string()
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let url = "postgres://postgres:postgrespass@localhost/poem_wizard_api";
-    let pool = sqlx::PgPool::connect(url).await?;
+    let url = database_url();
+    let pool = sqlx::PgPool::connect(&url).await?;
 
     sqlx::migrate!().run(&pool).await?;
     let _ = fs::create_dir("./files");
 
-    let api_service = OpenApiService::new(wizard_api::WizardApi, "Wizard API", "1.0.0")
-        .server("http://localhost:3000");
+    let api_service = OpenApiService::new(wizard_api::WizardApi, "Wizard API", "1.0.0");
 
     let ui = api_service.openapi_explorer();
 
